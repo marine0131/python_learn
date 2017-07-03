d880ef6b40c48329af5edba9af22800cd059bb73
haijun wang <whj0131@gmail.com>
2017-07-03 20:43:43 +0800

    add bottle exchange

---
+++ bottle_exchange.py
@@ -0,0 +1,14 @@
+import math
+empty=1000
+shell = 1000
+number = 1000
+def num(empty,shell):
+    return math.floor(empty/2)+math.floor(shell/4)
+
+while empty>=2 or shell>=4:
+    delta = num(empty,shell)
+    empty=empty%2+delta
+    shell=shell%4+delta
+    number=number+delta
+print(number)    
+
