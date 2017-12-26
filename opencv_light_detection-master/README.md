ee64b59c3bc3c4ae164aaae8129e4f97ebd0078c
Haijun Wang <wanghj@woosiyuan.com>
2017-12-26 17:57:10 +0800

    update

--- opencv_light_detection-master/README.md
+++
@@ -1,1 +0,0 @@
-# Detect oven status - OpenCV light detection
--- opencv_light_detection-master/test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-from bottle import route, run , request
-
-@route('/')
-def hello():
-    return "hello world"
-
-run(host='127.0.0.1', port=8080)
